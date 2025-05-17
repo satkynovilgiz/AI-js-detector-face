24c60c9b41c373a09bf7128f2db4f6f8ccc731e7
Ilgiz Satkynov <satkynovilgiz008@gmail.com>
2025-05-17 09:58:35 +0600

    fix: function

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function startVideo() {
     err => console.error(err)
   )
 }
-
+//funtion for AI
 video.addEventListener('play', () => {
   const canvas = faceapi.createCanvasFromMedia(video)
   document.body.append(canvas)
